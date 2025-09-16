bc68017b667335bff8b6cf9b98017a373557dfb1
to2bugs <102031006+to2bugs@users.noreply.github.com>
2025-09-16 10:14:27 +0800

    fix

---
+++ springmvc-09-best-practise-swagger/src/main/java/com/otto/springmvc/Springmvc09BestPractiseSwaggerApplication.java
@@ -0,0 +1,13 @@
+package com.otto.springmvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springmvc09BestPractiseSwaggerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Springmvc09BestPractiseSwaggerApplication.class, args);
+    }
+
+}
